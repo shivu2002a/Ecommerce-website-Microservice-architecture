b5ecbbfd7a7b8151e686c5ccc14cd73cb7d62dc4
Shivanagouda S A <shivu.a.1945@gmail.com>
2023-06-09 21:37:01 +0530

    Implementing APIs in order Service

---
+++ order-service/src/main/java/com/shivu/orderservice/model/Order.java
@@ -0,0 +1,35 @@
+package com.shivu.orderservice.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id ;
+
+    private String ordernumber;
+    
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> OrderLineItemsList;
+
+}
