bf6c76bda508229aa60c3667806f15a47082c1b6
Shivanagouda S A <shivu.a.1945@gmail.com>
2023-06-10 13:49:16 +0530

    Inventory service basic impl done

---
+++ inventory-service/src/main/java/com/shivu/inventoryservice/model/Inventory.java
@@ -0,0 +1,31 @@
+package com.shivu.inventoryservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="t_inventory")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Inventory {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String skuCode;
+    private Integer quantity;
+    
+    
+
+}
