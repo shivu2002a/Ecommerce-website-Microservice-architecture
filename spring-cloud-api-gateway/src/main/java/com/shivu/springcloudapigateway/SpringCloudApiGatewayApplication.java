5a03a28243a80971f5df7494968a135772418fe0
Shivanagouda S A <shivu.a.1945@gmail.com>
2023-06-13 16:59:50 +0530

    Added all the routes for Gateway

--- spring-cloud-api-gateway/src/main/java/com/shivu/springcloudapigateway/SpringCloudApiGatewayApplication.java
+++ spring-cloud-api-gateway/src/main/java/com/shivu/springcloudapigateway/SpringCloudApiGatewayApplication.java
@@ -2,10 +2,12 @@ package com.shivu.springcloudapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudApiGatewayApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudApiGatewayApplication.class, args);
 	}
